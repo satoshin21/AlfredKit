1b7039c9d49e9810096ca7d87637493d8e098d36
Satoshi Nagasaka <satoshi.nagasaka21@gmail.com>
2017-10-10 00:37:54 +0900

    Update tests

--- Tests/AlfredKitTests/AlfredKitTests.swift
+++ Tests/AlfredKitTests/AlfredKitTests.swift
@@ -8,16 +8,20 @@ class AlfredKitTests: XCTestCase {
         let alfred = Alfred()
         XCTAssertTrue(alfred.items.isEmpty)
 
+        
         let currentDir = alfred.fileManager.currentDirectoryPath
         XCTAssertFalse(currentDir.isEmpty)
         print("currentDir: \(currentDir)")
-        let process = ProcessInfo.processInfo
-        print("""
-            ARGUMENTS:
-            \(process.arguments)
-            """)
-    }
 
+        let jsonEncoder = JSONEncoder()
+        var items = [Item]()
+        items.append(Item(title: "foo"))
+        items.append(Item(title: "bar", type: .file))
+
+        let encoded = try! jsonEncoder.encode(items)
+        let encodedString = String(data: encoded, encoding: .utf8 )
+        print(encodedString!)
+    }
 
     static var allTests = [
         ("testEnvironmentVariables", testEnvironmentVariables),
