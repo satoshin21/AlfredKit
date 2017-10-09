229bd5acae31c44c702407a0371b42008c41680c
Satoshi Nagasaka <satoshi.nagasaka21@gmail.com>
2017-10-09 16:14:50 +0900

    define Alfred

---
+++ Sources/AlfredKit/Alfred.swift
@@ -0,0 +1,8 @@
+//
+//  Alfred.swift
+//  AlfredKitPackageDescription
+//
+//  Created by Satoshi Nagasaka on 2017/10/09.
+//
+
+import Foundation
--- Sources/AlfredKit/AlfredKit.swift
+++
@@ -1,3 +0,0 @@
-struct AlfredKit {
-    var text = "Hello, World!"
-}
